edacf55609990deff81935528da3fb276083647f
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-08-11 21:02:42 +0100

    Changes made

---
+++ app/(serviceProviderTabs)/_layout.jsx
@@ -0,0 +1,118 @@
+import {Image, View,Text} from "react-native";
+import {FontAwesome} from "@expo/vector-icons";
+import {Tabs} from "expo-router";
+import image from "../../constants/images"
+
+
+
+const  TabIcon = ({icon, color, name, focused}) =>{
+
+    const getIcon = () =>{
+        if (icon) {
+            return (
+                <Image
+                    source={icon}
+                    resizeMode={"contain"}
+                    style={{ width: 28, height: 28, tintColor: color }}
+                />
+            );
+        }
+
+        const iconName = name === "Post" ? "plus" : "bell";
+        return <FontAwesome name={iconName} size={28} color={color}/>
+    };
+    return (
+        <View style={{ alignItems: "center", justifyContent: "center", paddingVertical: 5 }}>
+            {getIcon()}
+            <Text
+                style={{
+                    color,
+                    fontSize: 12,
+                    fontWeight: focused ? "bold" : "normal",
+                    marginTop: 4,
+                }}
+            >
+                {name}
+            </Text>
+        </View>
+    );
+}
+
+const TabsLayout = () =>{
+    return(
+        <>
+            <Tabs
+                screenOptions={{
+                    tabBarShowLabel : false,
+                    tabBarActiveTintColor : "rgba(32, 130, 32, 1)",
+                    tabBarStyle:{
+                        backgroundColor : "#fff",
+                        borderTopWidth : 1,
+                        borderTopColor : "rgba(183, 183, 183, 1)",
+                        height : 90,
+                    }
+                }}
+            >
+                <Tabs.Screen
+                    name="serviceProviderHome"
+                    options={{
+                        title:"serviceProviderHome.jsx",
+                        headerShown : false,
+                        tabBarIcon :
+                            ({color, focused}) =>(
+                                <TabIcon
+                                    icon={image.Home}
+                                    color={color}
+                                    name={"Home"}
+                                    focused={focused}
+                                />
+                            )
+                    }}
+                />
+                <Tabs.Screen
+                    name="kwaft"
+                    options={{
+                        title : "kwaft",
+                        headerShown:false,
+                        tabBarIcon : ({color , focused}) =>
+                            <TabIcon
+                                icon={image.repeat}
+                                color={color}
+                                name={"My Kwafts"}
+                                focused={focused}
+                            />
+                    }}
+                />
+                <Tabs.Screen
+                    name="chat"
+                    options={{
+                        title : "chat",
+                        headerShown:false,
+                        tabBarIcon : ({color , focused}) =>
+                            <TabIcon
+                                icon={image.message}
+                                color={color}
+                                name={"Chat"}
+                                focused={focused}
+                            />
+                    }}
+                />
+                <Tabs.Screen
+                    name="profile"
+                    options ={{
+                        title : "profile",
+                        headerShown : false,
+                        tabBarIcon : ({color, focused }) =>
+                            <TabIcon
+                                icon={image.user}
+                                color={color}
+                                name={"Profile"}
+                                focused={focused}
+                            />
+                    }}
+                />
+            </Tabs>
+        </>
+    )
+}
+export default TabsLayout
